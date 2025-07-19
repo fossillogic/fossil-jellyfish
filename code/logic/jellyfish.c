0081eef69da551871e3c9d4f02526fcc32138634
Michael Gene Brockus (Dreamer) <55331536+dreamer-coding@users.noreply.github.com>
2025-07-18 22:52:54 -0600

    Update jellyfish.c

--- code/logic/jellyfish.c
+++ code/logic/jellyfish.c
@@ -19,6 +19,95 @@
 #include <ctype.h>
 #include <time.h>
 
+
+// Parses a string like: "key": "value"
+static bool match_key_value(const char **ptr, const char *key, const char *value) {
+    const char *p = *ptr;
+    while (isspace(*p)) p++;
+    size_t klen = strlen(key);
+    if (strncmp(p, "\"", 1) != 0 || strncmp(p + 1, key, klen) != 0 || strncmp(p + 1 + klen, "\":", 2) != 0)
+        return false;
+    p += klen + 3;
+    while (isspace(*p)) p++;
+    size_t vlen = strlen(value);
+    if (strncmp(p, "\"", 1) != 0 || strncmp(p + 1, value, vlen) != 0 || strncmp(p + 1 + vlen, "\"", 1) != 0)
+        return false;
+    *ptr = p + vlen + 2;
+    return true;
+}
+
+static bool skip_key(const char **ptr, const char *key) {
+    const char *p = *ptr;
+    while (isspace(*p)) p++;
+    size_t klen = strlen(key);
+    if (strncmp(p, "\"", 1) != 0 || strncmp(p + 1, key, klen) != 0 || strncmp(p + 1 + klen, "\":", 2) != 0)
+        return false;
+    *ptr = p + klen + 3;
+    return true;
+}
+
+static bool skip_symbol(const char **ptr, char symbol) {
+    const char *p = *ptr;
+    while (isspace(*p)) p++;
+    if (*p != symbol)
+        return false;
+    *ptr = p + 1;
+    return true;
+}
+
+static bool skip_comma(const char **ptr) {
+    const char *p = *ptr;
+    while (isspace(*p)) p++;
+    if (*p == ',') {
+        *ptr = p + 1;
+        return true;
+    }
+    return false;
+}
+
+static bool parse_string_field(const char **ptr, const char *key, char *out, size_t max) {
+    if (!skip_key(ptr, key)) return false;
+    if (!skip_symbol(ptr, '"')) return false;
+
+    const char *p = *ptr;
+    size_t i = 0;
+
+    while (*p && *p != '"' && i < max - 1) {
+        if (*p == '\\' && *(p + 1)) p++; // skip escape
+        out[i++] = *p++;
+    }
+
+    if (*p != '"') return false;
+    out[i] = '\0';
+    *ptr = p + 1;
+    return true;
+}
+
+static bool parse_number_field(const char **ptr, const char *key, double *out_d, uint64_t *out_u64, int *out_i, uint32_t *out_u32) {
+    if (!skip_key(ptr, key)) return false;
+
+    char *end;
+    const char *p = *ptr;
+
+    while (isspace(*p)) p++;
+
+    if (out_d) {
+        *out_d = strtod(p, &end);
+    } else if (out_u64) {
+        *out_u64 = strtoull(p, &end, 10);
+    } else if (out_i) {
+        *out_i = strtol(p, &end, 10);
+    } else if (out_u32) {
+        *out_u32 = strtoul(p, &end, 10);
+    } else {
+        return false;
+    }
+
+    if (end == p) return false;
+    *ptr = end;
+    return true;
+}
+
 void fossil_jellyfish_hash(const char *input, const char *output, uint8_t *hash_out) {
     const uint32_t FNV_PRIME = 0x01000193;
     uint32_t hash = 0x811c9dc5;
